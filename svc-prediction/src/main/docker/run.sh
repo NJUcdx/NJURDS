a4ef3de1582a6acd3dc16a5a697d3243cdf19d35
chendongxiao <MF21320014@smail.nju.edu.cn>
2022-01-28 17:10:10 +0800

    update test case

--- svc-prediction/src/main/docker/run.sh
+++ svc-prediction/src/main/docker/run.sh
@@ -12,11 +12,11 @@ echo "********************************************************"
 while ! `nc -z configserver $CONFIGSERVER_PORT `; do sleep 3; done
 echo ">>>>>>>>>>>> Configuration Server has started"
 
-echo "********************************************************"
-echo "Waiting for the database server to start on port $DATABASESERVER_PORT1"
-echo "********************************************************"
-while ! `nc -z ieeedb $DATABASESERVER_PORT1`; do sleep 3; done
-echo ">>>>>>>>>>>> Database Server has started"
+#echo "********************************************************"
+#echo "Waiting for the database server to start on port $DATABASESERVER_PORT1"
+#echo "********************************************************"
+#while ! `nc -z ieeedb $DATABASESERVER_PORT1`; do sleep 3; done
+#echo ">>>>>>>>>>>> Database Server has started"
 
 
 echo "********************************************************"
