dc135eae053a706f1cec1b85b85aeb48edbc8074
陈东骁 <mf21320014@smail.nju.edu.cn>
2022-03-31 19:35:00 +0800

    1

--- svc-recommend/src/test/java/com/nju/svcrecommend/controller/SLRController_ESTest.java
+++ svc-recommend/src/test/java/com/nju/svcrecommend/controller/SLRController_ESTest.java
@@ -22,26 +22,8 @@ import org.junit.runner.RunWith;
 @RunWith(EvoRunner.class) @EvoRunnerParameters(mockJVMNonDeterminism = true, useVFS = true, useVNET = true, resetStaticState = true, separateClassLoader = false, useJEE = true)
 public class SLRController_ESTest extends SLRController_ESTest_scaffolding {
 
-/**
+
   @Test(timeout = 4000)
   public void test0()  throws Throwable  {
-      ACMSLRMapper aCMSLRMapper0 = mock(ACMSLRMapper.class, new ViolatedAssumptionAnswer());
-      doReturn((ArrayList) null).when(aCMSLRMapper0).selectPaperVOsByKeyword(anyString() , anyInt() , anyInt());
-      IEEESLRMapper iEEESLRMapper0 = mock(IEEESLRMapper.class, new ViolatedAssumptionAnswer());
-      doReturn((ArrayList) null).when(iEEESLRMapper0).selectPaperVOsByKeyword(anyString() , anyInt() , anyInt());
-      SLRSvcImp sLRSvcImp0 = new SLRSvcImp(aCMSLRMapper0, iEEESLRMapper0);
-      SLRController sLRController0 = new SLRController(sLRSvcImp0);
-      // Undeclared exception!
-      try { 
-        sLRController0.getSLRRecommend("6_;U'`YzU3WR.5", 1590, 0, 1, 1, 1);
-        fail("Expecting exception: NullPointerException");
-      
-      } catch(NullPointerException e) {
-         //
-         // no message in exception (getMessage() returned null)
-         //
-         verifyException("com.nju.svcrecommend.serviceImp.SLRSvcImp", e);
-      }
   }
-  */
 }
